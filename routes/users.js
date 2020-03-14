d014983e30f661b81863245c9fa6be393076380f
Porson <porsonlee@gmail.com>
2020-03-14 01:10:23 -0700

    added register handle for POST when user register a new account and added bodyparser

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,11 @@ router.get('/login', (req, res) => res.render('login'));
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handle
+router.post('/register', (req, res) => {
+  console.log(req.body)
+  res.send('hello')
+})
 
 
 module.exports = router;
