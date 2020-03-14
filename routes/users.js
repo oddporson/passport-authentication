a9b6def9b889816052202e465fb1ecae7e6c1c42
Porson <porsonlee@gmail.com>
2020-03-14 01:40:01 -0700

    created conditioning that checks for password matching, password length and required fields

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,36 @@ router.get('/register', (req, res) => res.render('register'));
 
 // Register Handle
 router.post('/register', (req, res) => {
-  console.log(req.body)
-  res.send('hello')
+ const { name, email, password, password2 } = req.body;
+ let errors = [];
+
+ // Check required fields
+ if (!name || !email || !password || !password2) {
+  errors.push({ msg: 'Please fill in all fields' })
+ }
+
+ // Check if passwords match
+ if (password !== password2) {
+   errors.push({ msg: 'Passwords do not match' });
+ }
+
+ // Check password length
+ if (password.length < 6) {
+   errors.push({ msg: 'Password should be at least 6 characters' })
+ }
+
+ if(errors.length > 0) {
+  res.render('register', {
+    errors,
+    name, 
+    email,
+    password,
+    password2
+  });
+ } else {
+   res.send('pass');
+ }
+
 })
 
 
